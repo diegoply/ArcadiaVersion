70ddf64364435e643ae17e1d072828e732fce3ab
Diego Ply <ply.dgo@gmail.com>
2024-04-23 09:39:01 +0200

    finFixture User

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Role;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,7 +12,7 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-         $role1 = new Role();
+         /*$role1 = new Role();
          $role1->setLabel('role_admin');
          $manager->persist($role1);
 
@@ -21,8 +22,22 @@ class AppFixtures extends Fixture
 
          $role3 = new Role();
          $role3->setLabel('role_veterinaire');
-         $manager->persist($role3);
+         $manager->persist($role3)}
 
-         $manager->flush();
+        $role = $manager->getRepository(Role::class)->findOneBy(['label' => 'role_admin']);
+
+
+         $user1 = new User();
+         $user1->setEmail('test@admin.com')
+                ->setPassword('$2y$13$gm6lRe.v3v47O3/HL1NHX.aOfiicwxtFEfvaE2HBE0NHR2SQGm5pS')
+                ->setNom('Administrateur')
+                ->setPrenom('Personnel')
+                ->setRole($role);
+        $manager->persist($user1);
+        
+
+         $manager->flush();*/
+
+         
     }
 }
