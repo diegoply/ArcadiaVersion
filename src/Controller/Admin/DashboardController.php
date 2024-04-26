3b86c6c515fd068847ada61395d3b6541d2ec7ff
Diego Ply <ply.dgo@gmail.com>
2024-04-26 18:47:08 +0200

    avant résolution erreur image blob

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Image;
 use App\Entity\Race;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -42,8 +43,8 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');    
         yield MenuItem::linkToCrud('Races', 'fa-solid fa-horse', Race::class);
+        yield MenuItem::linkToCrud('Images', 'fa-regular fa-images', Image::class);
     }
 }
