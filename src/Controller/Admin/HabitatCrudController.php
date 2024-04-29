fb01defc96d4075fdcdea752b2d8c6712862047b
Diego Ply <ply.dgo@gmail.com>
2024-04-29 17:53:19 +0200

    ajout taille text habitat

--- src/Controller/Admin/HabitatCrudController.php
+++ src/Controller/Admin/HabitatCrudController.php
@@ -9,6 +9,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Validator\Constraints\Length;
 
 class HabitatCrudController extends AbstractCrudController
 {
@@ -21,8 +22,14 @@ class HabitatCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         yield TextField::new('nom');
-        yield TextareaField::new('description');
-        yield TextareaField::new('commentaireHabitat');
+        yield TextareaField::new('description')
+            ->setFormTypeOption('constraints', [
+                new Length(['max' => 50])
+            ]);
+        yield TextareaField::new('commentaireHabitat')
+            ->setFormTypeOption('constraints', [
+                new Length(['max' => 255])
+            ]);
         yield AssociationField::new('image');
     }
     
