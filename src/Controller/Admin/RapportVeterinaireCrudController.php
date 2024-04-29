cbdcef41862d7ccd8cf4bc7f8bcf07bebb74a0c4
Diego Ply <ply.dgo@gmail.com>
2024-04-29 19:06:20 +0200

    fin rapportVeterinaire easyadmin

--- src/Controller/Admin/RapportVeterinaireCrudController.php
+++ src/Controller/Admin/RapportVeterinaireCrudController.php
@@ -3,11 +3,16 @@
 namespace App\Controller\Admin;
 
 use App\Entity\RapportVeterinaire;
+use DateTime;
+use DateTimeInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class RapportVeterinaireCrudController extends AbstractCrudController
 {
@@ -20,6 +25,9 @@ class RapportVeterinaireCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         yield AssociationField::new('animal');
+        yield DateTimeField::new('date');
+        yield TextareaField::new('detail');
+
     }
     
 }
