78506887d270c56a4a86e268c185a31146a571eb
Diego Ply <ply.dgo@gmail.com>
2024-05-20 16:50:18 +0200

    appconfig

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -16,13 +16,13 @@ class AppFixtures extends Fixture
          $role1->setLabel('ROLE_ADMIN');
          $manager->persist($role1);
 
-         $role2 = new Role();
+        /* $role2 = new Role();
          $role2->setLabel('ROLE_EMPLOYER');
          $manager->persist($role2);
 
          $role3 = new Role();
          $role3->setLabel('ROLE_VETERINAIRE');
-         $manager->persist($role3);
+         $manager->persist($role3);*/
 
         
 
