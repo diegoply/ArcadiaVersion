cbb3b50565bc5f3c58f6470c9cfa2797f5d2396e
Diego Ply <ply.dgo@gmail.com>
2024-05-21 09:23:39 +0200

    ajout fixtures role et user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -16,17 +16,25 @@ class AppFixtures extends Fixture
          $role1->setLabel('ROLE_ADMIN');
          $manager->persist($role1);
 
-         $role2 = new Role();
+        /* $role2 = new Role();
          $role2->setLabel('ROLE_EMPLOYER');
          $manager->persist($role2);
 
          $role3 = new Role();
          $role3->setLabel('ROLE_VETERINAIRE');
-         $manager->persist($role3);
+         $manager->persist($role3);*/
 
-        
+        $user1 = new User();
+        $user1->setEmail('admin@example.com');
+        $user1->setNom('admin');
+        $user1->setPrenom('user');
+        $user1->setPassword('$2y$13$USI8CFUdueuRAhsdpD7BK.BVODMSAXK.Yxlz6BQlAQQuiIiq4194W');
+        $user1->setRole($role1);
+
+        $manager->persist($user1);
 
          $manager->flush();
+
     }
          
 }
